aaff5fe50b2adc4bf2a7b65963924691cf7d35d8
ledaniel0 <102328391+ledaniel0@users.noreply.github.com>
2023-06-01 10:54:23 -0700

    Update MovieLinkedList.java

--- MovieLinkedList.java
+++ MovieLinkedList.java
@@ -1,37 +1,68 @@
-package FinalProject
+package FinalProject;
 
 public class MovieLinkedList {
-    private Movie head;
+    private MovieNode head;
 
+    // Constructor to initialize the MovieLinkedList object
     public MovieLinkedList() {
-        this.head = null;
+        head = null;
     }
 
-    public boolean isEmpty() {
-        // Placeholder for checking if the linked list is empty
-        return head == null;
-    }
-
+    // Add a movie to the linked list
     public void addMovie(Movie movie) {
-        // Placeholder for adding a movie node to the linked list
-        // ...
+        MovieNode newNode = new MovieNode(movie);
+        if (head == null) {
+            head = newNode;
+        } else {
+            MovieNode current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
     }
 
+    // Remove a movie from the linked list
     public void removeMovie(Movie movie) {
-        // Placeholder for removing a movie node from the linked list
-        // ...
+        if (head == null) {
+            return;
+        }
+
+        if (head.movie == movie) {
+            head = head.next;
+        } else {
+            MovieNode current = head;
+            while (current.next != null && current.next.movie != movie) {
+                current = current.next;
+            }
+            if (current.next != null) {
+                current.next = current.next.next;
+            }
+        }
     }
 
+    // Traverse and print the linked list of movies
     public void traverse() {
-        // Placeholder for traversing the linked list
-        // ...
+        MovieNode current = head;
+        while (current != null) {
+            System.out.println("Movie: " + current.movie.getTitle());
+            System.out.println("Ticket Sales: " + current.movie.getTicketSales());
+            System.out.println("Revenue: $" + current.movie.getRevenue());
+            System.out.println("-------------------------");
+            current = current.next;
+        }
     }
 
-    public Movie searchMovie(String title) {
-        // Placeholder for searching a movie by title in the linked list
-        // ...
-        return null;
-    }
+    // Inner class representing a node in the linked list
+    private class MovieNode {
+        private Movie movie;
+        private MovieNode next;
 
-    // Other methods for manipulating the movie linked list
+        // Constructor to initialize the MovieNode object
+        public MovieNode(Movie movie) {
+            this.movie = movie;
+            this.next = null;
+        }
+    }
 }
+
