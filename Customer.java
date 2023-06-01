6b83c3681dc5cd8947576eb868cca72358a9268f
ledaniel0 <102328391+ledaniel0@users.noreply.github.com>
2023-06-01 10:51:37 -0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -3,42 +3,33 @@ package FinalProject;
 public class Customer {
     private String name;
     private String email;
-    private int seat;
 
     // Constructor to initialize the Customer object
-    public Customer(String name, String email, int seat) {
-        this.name = name;
-        this.email = email;
-        this.seat = seat;
-    }
-
-    // Getters and setters for the customer attributes
-     public Customer(String name, String email) {
+    public Customer(String name, String email) {
         this.name = name;
         this.email = email;
     }
 
     public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+        return this.name;
     }
 
     public String getEmail() {
-        return email;
+        return this.email;
     }
 
-    public void setEmail(String email) {
+    public void setEmail(String name) {
+        this.name = name;
+    }
+
+    public void setName(String email) {
         this.email = email;
     }
-    
-    public void setSeat(int seat){
-        this.seat = seat;
-    }
-    
-    public int getSeat(){
-        return seat;
+
+    @Override
+    public String toString() {
+        return "Name: " + this.name +
+                "\nEmail: " + this.email;
     }
 }
+
