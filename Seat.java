a781e0832ac350fdc701228514c490106cec0481
ledaniel0 <102328391+ledaniel0@users.noreply.github.com>
2023-05-31 22:22:34 -0700

    Update Seat.java

--- Seat.java
+++ Seat.java
@@ -3,13 +3,37 @@ package FinalProject;
 public class Seat {
     private int row;
     private int column;
+    private boolean available;
+    private Seat next;
 
-    // Constructor to initialize the Seat object
     public Seat(int row, int column) {
         this.row = row;
         this.column = column;
+        this.available = true;
+        this.next = null;
     }
 
-    // Getters and setters for the seat attributes
-    // ...
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public Seat getNext() {
+        return next;
+    }
+
+    public void setNext(Seat next) {
+        this.next = next;
+    }
 }
