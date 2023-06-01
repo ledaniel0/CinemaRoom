726975d4c014509c69dc4c0a0b897d7cbc6f2821
ledaniel0 <102328391+ledaniel0@users.noreply.github.com>
2023-05-31 20:29:00 -0700

    Update SeatLinkedLIst.java

--- SeatLinkedLIst.java
+++ SeatLinkedLIst.java
@@ -1,23 +1,37 @@
 package FinalProject;
 
-public class SeatLinkedList {
+class SeatLinkedList {
     private Seat head;
 
     public SeatLinkedList() {
-        
+        this.head = null;
     }
 
     public boolean isEmpty() {
-        
+        // Placeholder for checking if the linked list is empty
+        return head == null;
     }
 
     public void addSeat(Seat seat) {
-        
+        // Placeholder for adding a seat node to the linked list
+        // ...
     }
 
     public void removeSeat(Seat seat) {
-        
+        // Placeholder for removing a seat node from the linked list
+        // ...
     }
 
-    // Other methods for traversing, searching, and manipulating the seat linked list
+    public void traverse() {
+        // Placeholder for traversing the linked list
+        // ...
+    }
+
+    public Seat searchSeat(int row, int column) {
+        // Placeholder for searching a seat by row and column in the linked list
+        // ...
+        return null;
+    }
+
+    // Other methods for manipulating the seat linked list
 }
