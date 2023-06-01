68f069a60d626f3eb8ab057dd347f03452af0205
ledaniel0 <102328391+ledaniel0@users.noreply.github.com>
2023-05-31 22:20:45 -0700

    Update Movie.java

--- Movie.java
+++ Movie.java
@@ -4,14 +4,45 @@ public class Movie {
     private String title;
     private String director;
     private int duration;
+    public Movie next;
 
     // Constructor to initialize the Movie object
     public Movie(String title, String director, int duration) {
         this.title = title;
         this.director = director;
         this.duration = duration;
+        this.next = null;
     }
 
-    // Getters and setters for the movie attributes
-    // ...
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getDirector() {
+        return this.director;
+    }
+
+    public int getDuration() {
+        return this.duration;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie: " + this.title +
+                "\nDirector: " + this.director +
+                "\nDuration: " + this.duration + " minutes";
+    }
 }
+
