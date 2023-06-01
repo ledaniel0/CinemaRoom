5d3dafecb87f631e00fb82fff30f0df2a9409d9f
ledaniel0 <102328391+ledaniel0@users.noreply.github.com>
2023-05-31 20:20:21 -0700

    Update MovieLinkedList.java

--- MovieLinkedList.java
+++ MovieLinkedList.java
@@ -4,43 +4,19 @@ public class MovieLinkedList {
     private Movie head;
 
     public MovieLinkedList() {
-        this.head = null;
+        
     }
 
     public boolean isEmpty() {
-        return head == null;
+        
     }
 
     public void addMovie(Movie movie) {
-        if (isEmpty()) {
-            head = movie;
-        } else {
-            Movie current = head;
-            while (current.getNext() != null) {
-                current = current.getNext();
-            }
-            current.setNext(movie);
-        }
+        
     }
 
     public void removeMovie(Movie movie) {
-        if (isEmpty()) {
-            return;
-        }
         
-        if (head == movie) {
-            head = head.getNext();
-            return;
-        }
-
-        Movie current = head;
-        while (current.getNext() != null) {
-            if (current.getNext() == movie) {
-                current.setNext(movie.getNext());
-                break;
-            }
-            current = current.getNext();
-        }
     }
 
     // Other methods for traversing, searching, and manipulating the movie linked list
