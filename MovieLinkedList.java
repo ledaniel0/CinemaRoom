a8f248464182a0ece3d342dcbe1e48604fd482d7
ledaniel0 <102328391+ledaniel0@users.noreply.github.com>
2023-05-31 20:28:28 -0700

    Update MovieLinkedList.java

--- MovieLinkedList.java
+++ MovieLinkedList.java
@@ -1,23 +1,37 @@
 package FinalProject
 
-public class MovieLinkedList {
+class MovieLinkedList {
     private Movie head;
 
     public MovieLinkedList() {
-        
+        this.head = null;
     }
 
     public boolean isEmpty() {
-        
+        // Placeholder for checking if the linked list is empty
+        return head == null;
     }
 
     public void addMovie(Movie movie) {
-        
+        // Placeholder for adding a movie node to the linked list
+        // ...
     }
 
     public void removeMovie(Movie movie) {
-        
+        // Placeholder for removing a movie node from the linked list
+        // ...
     }
 
-    // Other methods for traversing, searching, and manipulating the movie linked list
+    public void traverse() {
+        // Placeholder for traversing the linked list
+        // ...
+    }
+
+    public Movie searchMovie(String title) {
+        // Placeholder for searching a movie by title in the linked list
+        // ...
+        return null;
+    }
+
+    // Other methods for manipulating the movie linked list
 }
