0a72720f2171c9ab34cdea06998537380e7bb98e
BP1009 <135192377+BP1009@users.noreply.github.com>
2023-06-01 19:16:25 -0700

    Update SeatLinkedLIst.java

--- SeatLinkedLIst.java
+++ SeatLinkedLIst.java
@@ -1,37 +1,66 @@
 package FinalProject;
 
-class SeatLinkedList {
-    private Seat head;
+  class SeatLinkedList {
+        private Seat head;
 
-    public SeatLinkedList() {
-        this.head = null;
+        public SeatLinkedList() {
+            this.head = null;
+        }
+
+        public boolean isEmpty() {
+            return head == null;
+        }
+
+        public void addSeat(Seat seat) {
+            if (isEmpty()) {
+                head = seat;
+            } else {
+                Seat current = head;
+                while (current.getNext() != null) {
+                    current = current.getNext();
+                }
+                current.setNext(seat);
+            }
+        }
+
+        public void removeSeat(Seat seat) {
+            if (head == null) {
+                return;
+            }
+
+            if (head == seat) {
+                head = head.getNext();
+            } else {
+                Seat current = head;
+                Seat previous = null;
+                while (current != null && current != seat) {
+                    previous = current;
+                    current = current.getNext();
+                }
+                if (current != null) {
+                    previous.setNext(current.getNext());
+                }
+            }
+        }
+
+        public void traverse() {
+            Seat current = head;
+            while (current != null) {
+                current = current.getNext();
+            }
+        }
+
+        public Seat searchSeat(int row, int column) {
+            Seat current = head;
+            while (current != null) {
+                if (current.getRow() == row && current.getColumn() == column) {
+                    return current;
+                }
+                current = current.getNext();
+            }
+            return null;
+        }
+
     }
-
-    public boolean isEmpty() {
-        // Placeholder for checking if the linked list is empty
-        return head == null;
-    }
-
-    public void addSeat(Seat seat) {
-        // Placeholder for adding a seat node to the linked list
-        // ...
-    }
-
-    public void removeSeat(Seat seat) {
-        // Placeholder for removing a seat node from the linked list
-        // ...
-    }
-
-    public void traverse() {
-        // Placeholder for traversing the linked list
-        // ...
-    }
-
-    public Seat searchSeat(int row, int column) {
-        // Placeholder for searching a seat by row and column in the linked list
-        // ...
-        return null;
-    }
-
-    // Other methods for manipulating the seat linked list
 }
+
