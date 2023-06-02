b0bb5c15b569975ac4904ddb8b1d756c205c6f3f
ledaniel0 <102328391+ledaniel0@users.noreply.github.com>
2023-06-01 21:12:10 -0700

    Update SeatLinkedLIst.java

--- SeatLinkedLIst.java
+++ SeatLinkedLIst.java
@@ -1,66 +1,65 @@
 package FinalProject;
 
-  class SeatLinkedList {
-        private Seat head;
+public class SeatLinkedList {
+    private Seat head;
 
-        public SeatLinkedList() {
-            this.head = null;
-        }
-
-        public boolean isEmpty() {
-            return head == null;
-        }
-
-        public void addSeat(Seat seat) {
-            if (isEmpty()) {
-                head = seat;
-            } else {
-                Seat current = head;
-                while (current.getNext() != null) {
-                    current = current.getNext();
-                }
-                current.setNext(seat);
-            }
-        }
-
-        public void removeSeat(Seat seat) {
-            if (head == null) {
-                return;
-            }
-
-            if (head == seat) {
-                head = head.getNext();
-            } else {
-                Seat current = head;
-                Seat previous = null;
-                while (current != null && current != seat) {
-                    previous = current;
-                    current = current.getNext();
-                }
-                if (current != null) {
-                    previous.setNext(current.getNext());
-                }
-            }
-        }
-
-        public void traverse() {
-            Seat current = head;
-            while (current != null) {
-                current = current.getNext();
-            }
-        }
-
-        public Seat searchSeat(int row, int column) {
-            Seat current = head;
-            while (current != null) {
-                if (current.getRow() == row && current.getColumn() == column) {
-                    return current;
-                }
-                current = current.getNext();
-            }
-            return null;
-        }
-
+    public SeatLinkedList() {
+        this.head = null;
     }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void addSeat(Seat seat) {
+        if (isEmpty()) {
+            head = seat;
+        } else {
+            Seat current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = seat;
+        }
+    }
+
+    public void removeSeat(Seat seat) {
+        if (head == null) {
+            return;
+        }
+
+        if (head == seat) {
+            head = head.next;
+        } else {
+            Seat current = head;
+            Seat previous = null;
+            while (current != null && current != seat) {
+                previous = current;
+                current = current.next;
+            }
+            if (current != null) {
+                previous.next = current.next;
+            }
+        }
+    }
+
+    public void traverse() {
+        Seat current = head;
+        while (current != null) {
+            current = current.next;
+        }
+    }
+
+    public Seat searchSeat(int row, int column) {
+        Seat current = head;
+        while (current != null) {
+            if (current.getRow() == row && current.getColumn() == column) {
+                return current;
+            }
+            current = current.next;
+        }
+        return null;
+    }
+
 }
 
