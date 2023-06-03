4613e46dc00a313a4fe0474eb8397f9f5ec237b7
ledaniel0 <102328391+ledaniel0@users.noreply.github.com>
2023-06-02 20:24:26 -0700

    Update Cinema.java

--- Cinema.java
+++ Cinema.java
@@ -1,5 +1,8 @@
 package FinalProject;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Cinema {
     private MovieLinkedList movies;
 
@@ -19,12 +22,11 @@ public class Cinema {
     public void sellTicket(Movie movie, Seat seat, Customer customer) {
         if (isSeatAvailable(movie, seat.getRow(), seat.getColumn())) {
             seat.setAvailable(false);
-            seat.setSold();
             seat.setCustomer(customer);
             movie.incrementTicketSales();
             movie.addToRevenue(movie.getTicketPrice());
             customer.setSeat(seat);
-            System.out.println("Ticket sold successfully. Thank You for Purchasing!\n");
+            System.out.println("Ticket sold successfully. Thank you for purchasing!\n");
         } else {
             System.out.println("Sorry, the seat is not available.");
         }
@@ -45,7 +47,7 @@ public class Cinema {
         }
         return false;
     }
-    
+
     public void showStatistics() {
         System.out.println("Ticket Sales Statistics:");
         System.out.println("-------------------------");
@@ -54,5 +56,117 @@ public class Cinema {
 
     public void printMovieSchedule() {
         System.out.println(movies.toString());
+    }
+
+    public void buyTicket(Scanner scanner) {
+        System.out.println("Enter the movie title:");
+        scanner.nextLine();
+        String movieTitle = scanner.nextLine();
+        Movie buyMovie = movies.searchMovie(movieTitle);
+        if (buyMovie != null) {
+            buyMovie.seats.printSeats();
+            System.out.println();
+            int row;
+            boolean isValidRow = false;
+            do {
+                System.out.println("What row would you like?");
+                try {
+                    row = scanner.nextInt();
+                    scanner.nextLine();
+                    if (row <= 10) {
+                        isValidRow = true;
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid input. Row should be an integer.");
+                    scanner.nextLine(); // Consume the invalid input
+                    row = -1; // Set row to an invalid value to trigger re-prompting
+                }
+            } while (!isValidRow);
+
+            int column;
+            boolean isValidColumn = false;
+            do {
+                System.out.println("What column would you like?");
+                try {
+                    column = scanner.nextInt();
+                    scanner.nextLine();
+                    if (column <= 20) {
+                        isValidColumn = true;
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid input. Column should be an integer.");
+                    scanner.nextLine(); // Consume the invalid input
+                    column = -1; // Set column to an invalid value to trigger re-prompting
+                }
+            } while (!isValidColumn);
+
+            Seat selectedSeat = new Seat(row, column, buyMovie);
+            System.out.println("What is your name?");
+            String name = scanner.nextLine();
+            System.out.println("What is your email?");
+            String email = scanner.nextLine();
+            Customer buyingCustomer = new Customer(name, email);
+            if (isSeatAvailable(buyMovie, row, column)) {
+                sellTicket(buyMovie, selectedSeat, buyingCustomer);
+            }
+        }
+    }
+
+    public boolean refundTicket(Scanner scanner) {
+        System.out.println("Enter the movie title:");
+        scanner.nextLine();
+        String movieTitle = scanner.nextLine();
+        Movie movie = movies.searchMovie(movieTitle);
+        if (movie != null) {
+            movie.seats.printSeats();
+            System.out.println();
+            int row;
+            boolean isValidRow = false;
+            do {
+                System.out.println("Enter the row number:");
+                try {
+                    row = scanner.nextInt();
+                    scanner.nextLine();
+                    if (row <= 10) {
+                        isValidRow = true;
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid input. Row should be an integer.");
+                    scanner.nextLine(); // Consume the invalid input
+                    row = -1; // Set row to an invalid value to trigger re-prompting
+                }
+            } while (!isValidRow);
+
+            int column;
+            boolean isValidColumn = false;
+            do {
+                System.out.println("Enter the column number:");
+                try {
+                    column = scanner.nextInt();
+                    scanner.nextLine();
+                    if (column <= 20) {
+                        isValidColumn = true;
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid input. Column should be an integer.");
+                    scanner.nextLine(); // Consume the invalid input
+                    column = -1; // Set column to an invalid value to trigger re-prompting
+                }
+            } while (!isValidColumn);
+
+            if (isSeatAvailable(movie, row, column)) {
+                System.out.println("The seat is already available. Cannot refund the ticket.\n");
+            } else {
+                if (refundTicket(movieTitle, row, column)) {
+                    System.out.println("Ticket refunded successfully.\n");
+                    return true;
+                } else {
+                    System.out.println("Sorry, the ticket cannot be refunded.\n");
+                }
+            }
+        } else {
+            System.out.println("Movie not found.");
+        }
+        return false;
     }
 }
